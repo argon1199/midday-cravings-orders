0063fe889d0149d16824443d5f9040fc1c64b89f
argon1199 <35390530+argon1199@users.noreply.github.com>
2026-09-11 00:15:45 +0800

    Create config.js

---
+++ frontend/config.js
@@ -0,0 +1,8 @@
+// ---------------------------------------------------------------------
+// Fill this in after you deploy the Apps Script backend as a Web App.
+// Deploy > New deployment > Web app > Execute as: Me > Who has access: Anyone
+// Then paste the resulting URL (it ends in /exec) below.
+// ---------------------------------------------------------------------
+const CONFIG = {
+  API_URL: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE',
+};
